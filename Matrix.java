1a80462e60a028240261688a4024b64449cd4bb7
Moiz Farooq <progrmoiz@gmail.com>
2022-01-29 22:43:03 +0500

    Added example of quartify function

--- Matrix.java
+++ Matrix.java
@@ -156,7 +156,7 @@ final public class Matrix {
         }
         break;
     }
-    
+
     return quarter;
   }
 
@@ -234,46 +234,51 @@ final public class Matrix {
 
   // test client
   public static void main(String[] args) {
-    double[][] d = { { 1, 2, 3 }, { 4, 5, 6 }, { 9, 1, 3 } };
+    double[][] d = { { 1, 2, 3, 3 }, { 4, 5, 6, 3 }, { 9, 1, 3, 4 }, { 1, 2, 3, 4 } };
     Matrix D = new Matrix(d);
+    Matrix Q = D.quarterify(1);
     D.show();
     System.out.println();
 
-    Matrix A = Matrix.random(5, 5);
-    A.show();
+    System.out.println("Quarter 1");
+    Q.show();
     System.out.println();
 
-    A.swap(1, 2);
-    A.show();
-    System.out.println();
+    // Matrix A = Matrix.random(5, 5);
+    // A.show();
+    // System.out.println();
 
-    Matrix B = A.transpose();
-    B.show();
-    System.out.println();
+    // A.swap(1, 2);
+    // A.show();
+    // System.out.println();
 
-    Matrix C = Matrix.identity(5);
-    C.show();
-    System.out.println();
+    // Matrix B = A.transpose();
+    // B.show();
+    // System.out.println();
 
-    A.plus(B).show();
-    System.out.println();
+    // Matrix C = Matrix.identity(5);
+    // C.show();
+    // System.out.println();
 
-    B.times(A).show();
-    System.out.println();
+    // A.plus(B).show();
+    // System.out.println();
 
-    // shouldn't be equal since AB != BA in general
-    System.out.println(A.times(B).eq(B.times(A)));
-    System.out.println();
+    // B.times(A).show();
+    // System.out.println();
 
-    Matrix b = Matrix.random(5, 1);
-    b.show();
-    System.out.println();
+    // // shouldn't be equal since AB != BA in general
+    // System.out.println(A.times(B).eq(B.times(A)));
+    // System.out.println();
 
-    Matrix x = A.solve(b);
-    x.show();
-    System.out.println();
+    // Matrix b = Matrix.random(5, 1);
+    // b.show();
+    // System.out.println();
 
-    A.times(x).show();
+    // Matrix x = A.solve(b);
+    // x.show();
+    // System.out.println();
+
+    // A.times(x).show();
 
   }
 }
